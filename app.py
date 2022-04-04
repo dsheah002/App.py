770b8fdf99b18062c2cad36a3446fedffa4937fb
dsheah002 <85600959+dsheah002@users.noreply.github.com>
2022-04-04 14:42:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -291,6 +291,6 @@ def delete_glue_type(glue_type_id):
 
 
 # Server configurations - REM CHANGE
-# app.run(debug=True, host='0.0.0.0', port=8080)
+# app.run(debug=True, host="api.ap-sg-1.icp.infineon.com", port=6443)
 if __name__ == "__main__":
-app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
+app.run(host="api.ap-sg-1.icp.infineon.com", port=6443, debug=True, threaded=True)
